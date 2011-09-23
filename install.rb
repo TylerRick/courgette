9b0d6d5d8855792ddbc2bee83acae17ad3756fcd
Tyler Rick <tyler@k3integrations.com>
2011-09-22 21:26:29 -0700

    Symlink the gem's public/courgette directory into the app's public/ directory automatically when the Engine is started, instead of in the install.rb file, since install.rb doesn't ever get called now that this library is being loaded as a gem instead of a plugin.

--- install.rb
+++
@@ -1,2 +0,0 @@
-puts "Courgette install script is adding a symlink to courgette/public/courgette inside your application's public directory"
-`cd public;ln -s ../vendor/plugins/courgette/public/courgette courgette`
--- lib/courgette/engine.rb
+++ lib/courgette/engine.rb
@@ -1,4 +1,9 @@
 module Courgette
   class Engine < Rails::Engine
+    initializer 'courgette.symlink_assets' do |app|
+      command = "ln -s #{config.root}/public/courgette #{Rails.root}/public/courgette"
+      #puts command
+      system command unless File.exist? "#{Rails.root}/public/courgette"
+    end
   end
 end
