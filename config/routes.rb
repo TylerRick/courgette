d0d552624c4f16bf61fc6143fcf14e4457b654ef
Tyler Rick <tyler@k3integrations.com>
2011-09-22 21:29:55 -0700

    Made these routes available in all environments, including production. If you want to limit access, use a before_filter or similar to ensure user is authorized.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
-unless Rails.env.production?
-  Rails.application.routes.draw do
-    resources :features, :only => :index
-    match "/features/view"     => 'features#view',     :as => :feature_view
-    match "/features/source"   => 'features#source',   :as => :feature_source
-    match "/features/treeview" => 'features#treeview', :as => :treeview
-  end
+Rails.application.routes.draw do
+  resources :features, :only => :index
+  match "/features/view"     => 'features#view',     :as => :feature_view
+  match "/features/source"   => 'features#source',   :as => :feature_source
+  match "/features/treeview" => 'features#treeview', :as => :treeview
 end
